af53d05df5f4a36c75ef21fadedbdfff3ce98237
Maxwell Quammy Ahorlu Oware <113618674+Quammy93@users.noreply.github.com>
2023-09-07 22:06:38 +0000

    authentic2

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Authentication module."""
+from typing import TypeVar
+
+from flask import request
+from typing import List
+
+
+class Auth:
+    """Authentication class."""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if a path requires auth.
+
+        Args:
+            path: path
+            excluded_paths: path that should be ignored
+        Returns: True if the path requires auth
+        """
+        if path is None or excluded_paths is None:
+            return True
+        elif len(excluded_paths) == 0:
+            return True
+
+        if path.endswith("/"):
+            if path in excluded_paths:
+                return False
+        else:
+            n_path = f"{path}/"
+            if n_path in excluded_paths:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Processes request and check for auth headers
+
+        Args:
+              request: request object
+        Return: header if it available or None
+        """
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Get the current user from the request object.
+
+        Args:
+            request: request object
+        Return: User or None
+        """
+        return None
