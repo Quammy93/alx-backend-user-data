59446a90028ec086fd17307f0ae44f869c07bdb8
Quammy93 <Quammy93@gmail.com>
2023-09-10 22:43:52 +0000

    good

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python3
 """
-Route for API
+Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
@@ -31,25 +32,29 @@ elif getenv('AUTH_TYPE') == 'session_db_auth':
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """not found handler"""
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """request unauthorized"""
+    """ Request unauthorized
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """request forbidden"""
+    """ Request forbidden
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before_request():
-    """before request"""
+    """ Before request
+    """
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                       '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth and auth.require_auth(request.path, excluded_paths):
